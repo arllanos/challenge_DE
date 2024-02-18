82521f2a771f5ed19723002f8fe8340055d85d74
Ariel Llanos <ariel.llanos@b-yond.com>
2024-02-17 21:58:11 -0300

    adding q3 functions with profiling

--- src/q3_memory.py
+++ src/q3_memory.py
@@ -1,4 +1,22 @@
+import pandas as pd
 from typing import List, Tuple
 
+
 def q3_memory(file_path: str) -> List[Tuple[str, int]]:
-    pass
+    # read data in chunks to be more memory efficient
+    chunk_size = 100  # adjust based on your system's memory
+    mention_counts = pd.Series(dtype="int")
+
+    for df_chunk in pd.read_json(file_path, lines=True, chunksize=chunk_size):
+
+        if 'mentionedUsers' in df_chunk.columns and not df_chunk['mentionedUsers'].isnull().all():
+            mentioned_df = df_chunk[['mentionedUsers']].explode('mentionedUsers')
+            mentioned_df['username'] = mentioned_df['mentionedUsers'].apply(lambda x: x.get('username') if isinstance(x, dict) else None)
+            mentioned_df.dropna(subset=['username'], inplace=True)
+            
+            # update counts by adding to the overall count
+            chunk_counts = mentioned_df['username'].value_counts()
+            mention_counts = mention_counts.add(chunk_counts, fill_value=0)
+
+    top_10_mentions = mention_counts.nlargest(10).items()
+    return list(top_10_mentions)
--- src/q3_time.py
+++ src/q3_time.py
@@ -1,4 +1,19 @@
+import pandas as pd
 from typing import List, Tuple
 
+
 def q3_time(file_path: str) -> List[Tuple[str, int]]:
-    pass
+    # load the entire dataset at once
+    df = pd.read_json(file_path, lines=True)
+
+    if ('mentionedUsers' not in df.columns or df['mentionedUsers'].isnull().all()):
+        return []
+    
+    mentioned_df = df[['mentionedUsers']].explode('mentionedUsers')
+    mentioned_df['username'] = mentioned_df['mentionedUsers'].apply(lambda x: x.get('username') if isinstance(x, dict) else None)
+    mentioned_df.dropna(subset=['username'], inplace=True)
+
+    # count occurrences of each username
+    mentions_count = mentioned_df['username'].value_counts().head(10)
+
+    return list(mentions_count.items())
